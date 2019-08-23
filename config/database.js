ac0cce8d2d07906da23c87463a33eeda3afae3c8
Admin <Admin@MacBook-Pro-2.local>
2019-08-23 14:39:05 -0500

    db functional

--- config/database.js
+++ config/database.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 
+
 mongoose.connect(process.env.DATABASE_URL,
     {useNewUrlParser: true}
 );
-
-// mongoose.connection.on('connected', function () {
-//     console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
-//   });
+var db = mongoose.connection;
+mongoose.connection.on('connected', function () {
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`);
+  });
 
 module.exports = mongoose;
