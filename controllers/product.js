bb223cee3277f12842d4b42babafc0aa0e85e237
Admin <Admin@MacBook-Pro-2.local>
2019-08-28 15:19:31 -0500

    more styling done

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,20 @@
-var Products = require('../models/product');
+var Product = require('../models/product');
 
 module.exports = {
-    
+    index,
+    show,
+  
+};
+
+function index(req, res) {
+    Product.find({}, function(err, product) {
+        res.render('index', { product });
+
+    });
+}
+
+function show(req, res) {
+    Product.findById(req.params.id, function(err, product) {
+        res.render('products/product', {});
+    });
 }
--- routes/products.js
+++ routes/products.js
@@ -1,4 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var cartCtrl = require('../controllers/carts')
+
+/* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+// router.get('/', cartCtrl.index);
+// router.get('/new', cartCtrl.new);
+// router.post('/', cartCtrl.create);
+// //need to add a router.put('/:id', cartCtrl.update);
+// router.delete('/:id', cartCtrl.delete);
+
+
 module.exports = router;
